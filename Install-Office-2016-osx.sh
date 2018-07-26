98486758d5dd8eba5a48841c414f02adfeace5bd
nuclearsalt <41658789+nuclearsalt@users.noreply.github.com>
2018-07-26 11:19:17 -0400

    added more steps to the process
    can't figure out why the git master download isn't working. May need to figure out a different way, or learn more about the way git handles downloads.

--- Install-Office-2016-osx.sh
+++ Install-Office-2016-osx.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
 ##### This script will be used to Install Microsoft Office 2016.
 
+sudo -S mkdir /MS\ Office
+cd /MS\ Office/
 
-cd ~/Downloads
-#curl -O https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_Office_2016_16.15.18070902_Installer.pkg
-sudo /usr/sbin/installer -pkg '${HOME}/Downloads/Microsoft_Office_2016_16.15.18070902_Installer.pkg' -target '/Volumes/Macintosh HD/' -applyChoiceChangesXML choices.plist
-
-
-#    exit 0
+# The link below doesn't seem to be working. 
+# Using this link seems to create a master that just opens itself infinitely when double-clicked: https://github.com/nuclearsalt/install-office-2016-osx/archive/master.zip
+# sudo -S curl -O https://codeload.github.com/nuclearsalt/install-office-2016-osx/zip/master
+open master.zip
+cd /MS\ Office/install-office-2016-osx-master/
+sudo -S curl -O https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_Office_2016_16.15.18070902_Installer.pkg
+sudo -S /usr/sbin/installer -pkg '/MS\ Office/install-office-2016-osx-master/Microsoft_Office_2016_16.15.18070902_Installer.pkg' -target '/Volumes/Macintosh HD/' -applyChoiceChangesXML /MS\ Office/install-office-2016-osx-master/choices.plist
